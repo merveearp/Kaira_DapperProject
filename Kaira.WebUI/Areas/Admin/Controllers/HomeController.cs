9894a87277f35d64c0a6044106660a29aa59a062
Merve Arpacıoğlu Türk <merveearp@icloud.com>
2026-01-08 02:32:18 +0300

    End.

--- Kaira.WebUI/Areas/Admin/Controllers/HomeController.cs
+++ Kaira.WebUI/Areas/Admin/Controllers/HomeController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Kaira.WebUI.Repositories.CategoryRepositories;
+using Kaira.WebUI.Repositories.ProductRepositories;
+using Kaira.WebUI.Repositories.WearRepositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Kaira.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class HomeController : Controller
+    public class HomeController(ICategoryRepository _categoryRepository,IProductRepository _productRepository,IWearRepository _wearRepository ) : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            ViewBag.CountCategory = (await _categoryRepository.GetAllAsync()).Count();
+            ViewBag.CountProduct = (await _productRepository.GetAllAsync()).Count();
+            ViewBag.CountWear = (await _wearRepository.GetAllAsync()).Count();
+
             return View();
         }
     }
---
+++ Kaira.WebUI/Areas/Admin/ViewComponents/Home/_AdminRecentBlogComponent.cs
@@ -0,0 +1,17 @@
+﻿using Kaira.WebUI.Repositories.BlogRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kaira.WebUI.Areas.Admin.ViewComponents.Home
+{
+    public class _AdminRecentBlogComponent(IBlogRepository blogRepository) :ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = (await blogRepository.GetAllAsync())
+                .OrderByDescending(x=>x.BlogId)
+                .Take(4)
+                .ToList();
+            return View(values);
+        }
+    }
+}
